4c90faa237e75b10c4b36aa4fda355044df13ada
AJAY KUMAR <99399155+ajaydevtron@users.noreply.github.com>
2025-01-30 13:16:18 +0530

    Update main.tf

--- ec2_module/main.tf
+++ ec2_module/main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 
 resource "aws_instance" "server" {
   ami           = "ami-05fa46471b02db0ce"
-  instance_type = "t2-micro"
+  instance_type = "t2.micro"
   #   instance_count = var.number_of_instances
   tags = {
     Name        = "terraform-ec2"
