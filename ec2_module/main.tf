4caaca982ac97dc3d8ea583bcc6c9f712c24df22
AJAY KUMAR <99399155+ajaydevtron@users.noreply.github.com>
2025-01-30 13:13:09 +0530

    Update and rename ec2.tf to main.tf

--- ec2_module/main.tf
+++ ec2_module/main.tf
@@ -1,3 +1,8 @@
+provider "aws" {
+  region = "ap-south-1"  # Change to your desired AWS region
+}
+
+
 resource "aws_instance" "server" {
   ami           = "ami-05fa46471b02db0ce"
   instance_type = "t2-micro"
