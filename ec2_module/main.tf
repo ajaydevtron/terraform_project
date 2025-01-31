b1f8bc25b680b6b37919160e06cef70f6b92be92
AJAY KUMAR <99399155+ajaydevtron@users.noreply.github.com>
2025-01-31 11:45:46 +0530

    Update main.tf

--- ec2_module/main.tf
+++ ec2_module/main.tf
@@ -6,8 +6,10 @@ provider "aws" {
 resource "aws_instance" "server" {
   ami           = var.image_id
   instance_type = var.instance-type
+  #   instance_count = var.number_of_instances
   tags = {
     Name        = var.ec2_instance_name
     Environment = var.env_name
+  }
 }
 
